e2c0a6e55e05faee04d15201f08303df5bd4034f
David Cobb <david@davidcobb.net>
2020-03-26 12:31:10 -0400

    update install options for PS7 release

--- 00 ps7-installation.ps1
+++ 00 ps7-installation.ps1
@@ -1,12 +1,23 @@
 # INSTALLING POWERSHELL 7
 
-# Option 1: (RECOMMENDED) Install with PSReleaseTools
-Find-Module PSReleaseTools 
-Install-Module PSReleaseTools #as Admin or with -Scope CurrentUser
-Install-PSPreview -Mode Passive
+# Many options listed here
+# https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-windows?view=powershell-7
 
-# Option 2: Install manually from GitHub:
-# https://github.com/PowerShell/PowerShell
+# Option I chose and recommend if you're using .Net Core
 
-# Look for PowerShell 7-Preview in Start Menu
-# or run pwsh-preview
+# Install as a .NET Global tool
+
+# First install the .net Core SDK
+# https://dotnet.microsoft.com/download
+
+# Then use the dotnet command line 
+
+dotnet tool install --global PowerShell
+
+# See what is installed already
+
+dotnet tool list -g
+
+# See other dotnet tools available online 
+# https://github.com/natemcmaster/dotnet-tools
+
--- _my-settings.ps1
+++ _my-settings.ps1
@@ -1,5 +1,5 @@
 # To make these scripts more flexible, I'm defining variables here that may be used in multiple scripts
-$myResourceGroupName = 'sfsdc-rg'
+$myResourceGroupName = 'pass-rg'
 $myLocation = 'eastus2'
-$myStorageAccountName = 'sfsdcstor'
+$myStorageAccountName = 'passstor123'
 $myImageUrl = 'https://www.mcsfug.org/wp-content/uploads/2019/10/MCSF-Blue1-400x230.png'
